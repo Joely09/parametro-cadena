1ddf6fbc81de083bb4d00307633852127b469e9a
Joely09 <82679985+Joely09@users.noreply.github.com>
2021-10-08 20:07:28 -0400

    Add files via upload

---
+++ parametro de cadena/parametro de cadena/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace parametro_de_cadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa de cadena\n");
+            Console.WriteLine("Escribe una cadena\n");
+            string cadena = Console.ReadLine();
+            string[] answer = new string[0];
+            int f = 1;
+            string[] arreglo= cadena.Split(" ");
+           
+            for(int i = 0;i<arreglo.Length;i++)
+            {
+                Console.WriteLine("Palabra {0}: {1}",i,arreglo[i]);
+
+
+
+            }
+            
+        }
+    }
+}
